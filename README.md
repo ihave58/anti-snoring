33995cdcb8200f1c80ea42c71dd4636797c0f6c5
aadityakumar <aaditya.kumar@vedantu.com>
2017-05-26 19:47:26 +0530

    - Implementing the partial basic UI.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# anti-snoring
+# Anti Snoring
 App to help people to prevent snoring.
--- app/src/main/java/com/aadityakumar/antisnoring/MainActivity.java
+++ app/src/main/java/com/aadityakumar/antisnoring/MainActivity.java
@@ -1,52 +1,102 @@
 package com.aadityakumar.antisnoring;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+enum ServiceStatus {
+    Stopped,
+    Running
+}
 
 public class MainActivity extends AppCompatActivity {
+    protected ServiceStatus serviceStatus = ServiceStatus.Stopped;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(getFloatingActionButtonClickListener());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int itemId = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (itemId) {
+            case R.id.action_about_us:
+                break;
+
+            case R.id.action_settings:
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                break;
+
+            default:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private View.OnClickListener getFloatingActionButtonClickListener() {
+
+        return (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+                if (serviceStatus == ServiceStatus.Running) {
+                    serviceStatus = ServiceStatus.Stopped;
+
+                    fab.setBackgroundTintList(
+                            ColorStateList.valueOf(
+                                    ResourcesCompat.getColor(
+                                            getResources(), android.R.color.holo_red_dark, getTheme()
+                                    )
+                            )
+                    );
+                    fab.setImageResource(android.R.drawable.ic_media_pause);
+                    showMessage("Anti Snoring is stopped!");
+                } else {
+                    serviceStatus = ServiceStatus.Running;
+
+                    fab.setBackgroundTintList(
+                            ColorStateList.valueOf(
+                                    ResourcesCompat.getColor(
+                                            getResources(), android.R.color.holo_green_dark, getTheme()
+                                    )
+                            )
+                    );
+                    fab.setImageResource(android.R.drawable.ic_media_play);
+                    showMessage("Anti Snoring is running now...");
+                }
+            }
+        });
+    }
+
+    private void showMessage(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG)
+                .show();
     }
 }
